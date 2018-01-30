4483e5b7b8296a7532f76c03b90df2ab96e19f23
Clark Walker <303walker@gmail.com>
2018-01-30 10:00:20 -0700

    update

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
+// import routes/crud.js and router
 
 const path = require('path');
+
+// use routes ...
 
 app.listen(port, function() {
   console.log(`listening on ${port}`);
